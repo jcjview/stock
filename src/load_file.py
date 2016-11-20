3404d70b30aac180ac2ae0557e8d1a2992bf5337
jin_chongjiu <jin_chongjiu@gowild.cn>
2016-11-20 17:13:13 +0800

    push csv

---
+++ src/load_file.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+#coding:utf8
+
+"""
+@version: 0.1
+@author: jin_chongjiu
+@license: 
+@contact: 
+@site: http://www.gowild.com
+@software: PyCharm
+@file: load_file.py
+@time: 16/11/19 下午7:41
+"""
+
+
+def func():
+    pass
+
+
+class Main():
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
---
+++ src/panda.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+#coding:utf8
+
+"""
+@version: 0.1
+@author: jin_chongjiu
+@license: 
+@contact: 
+@site: http://www.gowild.com
+@software: PyCharm
+@file: panda.py
+@time: 16/11/19 下午8:14
+"""
+
+
+def func():
+    pass
+
+
+class Main():
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
